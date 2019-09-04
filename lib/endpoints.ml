6312e6a52fea3454cc18b1b1aa824d2a1be34b1e
Tim McGilchrist <timmcgil@gmail.com>
2019-09-05 08:13:24 +1000

    Re-run endpoint generation.

--- lib/endpoints.ml
+++ lib/endpoints.ml
@@ -1919,6 +1919,7 @@ let endpoint_of svc_name region =
       | "us-west-1" -> Some "xray.us-west-1.amazonaws.com"
       | "us-west-2" -> Some "xray.us-west-2.amazonaws.com"
       | _ -> None)
+
   | _ -> None
 
 let url_of svc_name region =
