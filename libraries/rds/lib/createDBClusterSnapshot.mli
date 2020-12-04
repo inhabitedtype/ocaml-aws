7ba6ae43b792d822ccdde3f8acc57494d9b1eff7
Tim McGilchrist <timmcgil@gmail.com>
2020-12-04 16:53:37 +1100

    Regenerate RDS with newer botocore definitions.

--- libraries/rds/lib/createDBClusterSnapshot.mli
+++ libraries/rds/lib/createDBClusterSnapshot.mli
@@ -1,10 +1,7 @@
 open Types
-
 type input = CreateDBClusterSnapshotMessage.t
-
 type output = CreateDBClusterSnapshotResult.t
-
 type error = Errors_internal.t
-
 include
-  Aws.Call with type input := input and type output := output and type error := error
+  Aws.Call with type  input :=  input and type  output :=  output and type
+     error :=  error
