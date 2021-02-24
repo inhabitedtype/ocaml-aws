db265ef8b0be98bac91f228e65c2e335f09bfa48
Tim McGilchrist <timmcgil@gmail.com>
2021-02-24 14:06:35 +1100

    Regenerate STS with minor hand modifications to module names.

--- libraries/sts/lib/getAccessKeyInfo.mli
+++ libraries/sts/lib/getAccessKeyInfo.mli
@@ -1,6 +1,6 @@
 open Types
-type input = DecodeAuthorizationMessageRequest.t
-type output = DecodeAuthorizationMessageResponse.t
+type input = GetAccessKeyInfoRequest.t
+type output = GetAccessKeyInfoResponse.t
 type error = Errors_internal.t
 include
   Aws.Call with type  input :=  input and type  output :=  output and type
