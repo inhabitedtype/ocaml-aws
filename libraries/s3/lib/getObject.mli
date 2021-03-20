45926c4a216e9f49a4379d6977ad63aec5cd0d33
Tim McGilchrist <timmcgil@gmail.com>
2021-03-21 08:30:03 +1100

    Remove generated code.

--- libraries/s3/lib/getObject.mli
+++
@@ -1,7 +0,0 @@
-open Types
-type input = GetObjectRequest.t
-type output = GetObjectOutput.t
-type error = Errors_internal.t
-include
-  Aws.Call with type  input :=  input and type  output :=  output and type
-     error :=  error
--- libraries/s3/lib/listObjects.mli
+++
@@ -1,7 +0,0 @@
-open Types
-type input = ListObjectsRequest.t
-type output = ListObjectsOutput.t
-type error = Errors_internal.t
-include
-  Aws.Call with type  input :=  input and type  output :=  output and type
-     error :=  error
