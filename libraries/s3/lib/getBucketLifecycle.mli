78f5549d6c8ed2320d8201150d7caedfbfae5816
Tim McGilchrist <timmcgil@gmail.com>
2021-03-21 08:30:03 +1100

    Remove generated code.

--- libraries/s3/lib/getBucketLifecycle.mli
+++
@@ -1,7 +0,0 @@
-open Types
-type input = GetBucketLifecycleRequest.t
-type output = GetBucketLifecycleOutput.t
-type error = Errors_internal.t
-include
-  Aws.Call with type  input :=  input and type  output :=  output and type
-     error :=  error
--- libraries/s3/lib/uploadPartCopy.mli
+++
@@ -1,7 +0,0 @@
-open Types
-type input = UploadPartCopyRequest.t
-type output = UploadPartCopyOutput.t
-type error = Errors_internal.t
-include
-  Aws.Call with type  input :=  input and type  output :=  output and type
-     error :=  error
