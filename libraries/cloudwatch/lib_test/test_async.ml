916bacdca19e89ee07e9b0c07978bc20e8dc41c5
Tim McGilchrist <timmcgil@gmail.com>
2021-04-22 17:28:09 +1000

    Rename the Unix Env variables expected in tests.
    They now match the AWS CLI supported environment variables.
    https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html

--- libraries/cloudwatch/lib_test/test_async.ml
+++ libraries/cloudwatch/lib_test/test_async.ml
@@ -3,8 +3,8 @@ open Aws_cloudwatch_test
 module T = TestSuite(struct
     type 'a m = 'a Async.Deferred.t
 
-    let access_key = Unix.getenv "AWS_ACCESS_KEY"
-    let secret_key = Unix.getenv "AWS_SECRET_KEY"
+    let access_key = Unix.getenv "AWS_ACCESS_KEY_ID"
+    let secret_key = Unix.getenv "AWS_SECRET_ACCESS_KEY"
     let region = Unix.getenv "AWS_DEFAULT_REGION"
 
     let run_request x = Aws_async.Runtime.run_request ~region ~access_key ~secret_key x
